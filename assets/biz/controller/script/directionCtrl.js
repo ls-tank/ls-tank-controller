9dab228c32152a8fabdbe6d5987f8419dc87a989
lishengzxc <eric@lishengcn.cn>
2016-02-03 00:10:29 +0800

    try calc the angle & Radians

--- assets/biz/controller/script/directionCtrl.js
+++ assets/biz/controller/script/directionCtrl.js
@@ -10,6 +10,7 @@ cc.Class({
     },
 
     onLoad: function () {
+        
         this.setTouchControl();
     },
     
@@ -17,19 +18,19 @@ cc.Class({
         var self = this;
         cc.eventManager.addListener({
             event: cc.EventListener.TOUCH_ONE_BY_ONE,
-            onTouchBegan: self.onTouchBegan,
-            onTouchMoved: self.onTouchMoved,
-            onTouchEnded: self.onTouchEnded
+            onTouchBegan: self.onTouchBegan.bind(self),
+            onTouchMoved: self.onTouchMoved.bind(self),
+            onTouchEnded: self.onTouchEnded.bind(self)
         }, self.node);
     },
     
     __getAngle: function (pos) {
-        this.angle = Math.atan2(pos.y, pos.x) * 57.29577951;
+        this.angle = Math.atan2(pos.y, pos.x) * 57.29577951; // todo
         return this.angle;
     },
     
     __getRadians: function (pos) {
-        this.radians = cc.PI / 180 * this._getAngle(pos);
+        this.radians = cc.PI / 180 * this.__getAngle(pos);
         return this.radians;
     },
     
@@ -37,11 +38,63 @@ cc.Class({
         return Math.sqrt(pos.x * pos.x + pos.y * pos.y);
     },
     
+    onUpdateDirection : function (position) {
+        // if(this.angle > -22.5 && this.angle < 22.5){
+        //     this.direction = 'D_RIGHT';
+        // }
+        // else if(this.angle > 22.5 && this.angle < 67.5){
+        //     this.direction = 'D_RIGHT_UP';
+        // }
+        // else if(this.angle > 67.5 && this.angle < 112.5){
+        //     this.direction = 'D_UP';
+        // }
+        // else if(this.angle > 112.5 && this.angle < 157.5){
+        //     this.direction = 'D_LEFT_UP';
+        // }
+        // else if((this.angle > 157.5 && this.angle < 180)||(this.angle < -157.5 && this.angle > -180)){
+        //     this.direction = 'D_LEFT';
+        // }
+        // else if(this.angle < -112.5 && this.angle > -157.5){
+        //     this.direction = 'D_LEFT_DOWN';
+        // }
+        // else if(this.angle < -67.5 && this.angle > -112.5){
+        //     this.direction = 'D_DOWN';
+        // }
+        // else if(this.angle < -22.5 && this.angle > -67.5){
+        //     this.direction = 'D_RIGHT_DOWN';
+        // }
+    },
+    
     onTouchBegan: function (touch, event) {
+        var target = this.follower;
+        var touchLoc = touch.getLocation();
+        
+        var locInNode = target.convertToNodeSpace(touchLoc);
+        
+        var size = target.parent.getContentSize();
+        size.width = size.width * target.parent.scale;
+        size.height = size.height * target.parent.scale;
+        
+        var rect = cc.rect(0, 0, size.width, size.height);
+        if (!cc.rectContainsPoint(rect, locInNode)) {
+            return false;
+        }
+        
         return true;
     },
     
     onTouchMoved: function (touch, event) {
+        var target = this.follower;
+        var touchLoc = touch.getLocation();
+        
+        var locInNode = target.convertToNodeSpace(touchLoc);
+        
+        this.__getAngle(locInNode);
+        this.__getRadians(locInNode);
+        this.onUpdateDirection(locInNode);
+        console.log(this.direction);
+        
+        
     },
     
     onTouchEnded: function (touch, event) {
--- library/imports/8d/8dfd1d48-fe90-400f-b5e7-378887f6d895.js
+++ library/imports/8d/8dfd1d48-fe90-400f-b5e7-378887f6d895.js
@@ -12,6 +12,7 @@ cc.Class({
     },
 
     onLoad: function onLoad() {
+
         this.setTouchControl();
     },
 
@@ -19,19 +20,19 @@ cc.Class({
         var self = this;
         cc.eventManager.addListener({
             event: cc.EventListener.TOUCH_ONE_BY_ONE,
-            onTouchBegan: self.onTouchBegan,
-            onTouchMoved: self.onTouchMoved,
-            onTouchEnded: self.onTouchEnded
+            onTouchBegan: self.onTouchBegan.bind(self),
+            onTouchMoved: self.onTouchMoved.bind(self),
+            onTouchEnded: self.onTouchEnded.bind(self)
         }, self.node);
     },
 
     __getAngle: function __getAngle(pos) {
-        this.angle = Math.atan2(pos.y, pos.x) * 57.29577951;
+        this.angle = Math.atan2(pos.y, pos.x) * 57.29577951; // todo
         return this.angle;
     },
 
     __getRadians: function __getRadians(pos) {
-        this.radians = cc.PI / 180 * this._getAngle(pos);
+        this.radians = cc.PI / 180 * this.__getAngle(pos);
         return this.radians;
     },
 
@@ -39,11 +40,62 @@ cc.Class({
         return Math.sqrt(pos.x * pos.x + pos.y * pos.y);
     },
 
+    onUpdateDirection: function onUpdateDirection(position) {
+        // if(this.angle > -22.5 && this.angle < 22.5){
+        //     this.direction = 'D_RIGHT';
+        // }
+        // else if(this.angle > 22.5 && this.angle < 67.5){
+        //     this.direction = 'D_RIGHT_UP';
+        // }
+        // else if(this.angle > 67.5 && this.angle < 112.5){
+        //     this.direction = 'D_UP';
+        // }
+        // else if(this.angle > 112.5 && this.angle < 157.5){
+        //     this.direction = 'D_LEFT_UP';
+        // }
+        // else if((this.angle > 157.5 && this.angle < 180)||(this.angle < -157.5 && this.angle > -180)){
+        //     this.direction = 'D_LEFT';
+        // }
+        // else if(this.angle < -112.5 && this.angle > -157.5){
+        //     this.direction = 'D_LEFT_DOWN';
+        // }
+        // else if(this.angle < -67.5 && this.angle > -112.5){
+        //     this.direction = 'D_DOWN';
+        // }
+        // else if(this.angle < -22.5 && this.angle > -67.5){
+        //     this.direction = 'D_RIGHT_DOWN';
+        // }
+    },
+
     onTouchBegan: function onTouchBegan(touch, event) {
+        var target = this.follower;
+        var touchLoc = touch.getLocation();
+
+        var locInNode = target.convertToNodeSpace(touchLoc);
+
+        var size = target.parent.getContentSize();
+        size.width = size.width * target.parent.scale;
+        size.height = size.height * target.parent.scale;
+
+        var rect = cc.rect(0, 0, size.width, size.height);
+        if (!cc.rectContainsPoint(rect, locInNode)) {
+            return false;
+        }
+
         return true;
     },
 
-    onTouchMoved: function onTouchMoved(touch, event) {},
+    onTouchMoved: function onTouchMoved(touch, event) {
+        var target = this.follower;
+        var touchLoc = touch.getLocation();
+
+        var locInNode = target.convertToNodeSpace(touchLoc);
+
+        this.__getAngle(locInNode);
+        this.__getRadians(locInNode);
+        this.onUpdateDirection(locInNode);
+        console.log(this.direction);
+    },
 
     onTouchEnded: function onTouchEnded(touch, event) {},
 
