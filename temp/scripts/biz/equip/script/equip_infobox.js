e84e18a7675710e85263f408b27f8b23439214b9
lishengzxc <eric@lishengcn.cn>
2016-04-02 16:02:01 +0800

    clear contents

--- temp/scripts/biz/equip/script/equip_infobox.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-cc._RFpush(module, '992f3VMJUpHLozQRabAZi9p', 'equip_infobox');
-// biz/equip/script/equip_infobox.js
-
-
-
-cc._RFpop();
--- temp/scripts/lib/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-cc._RFpush(module, '089aaTNnbBBA5p0EPcRxU5w', 'utils');
-// lib/utils.js
-
-
-
-cc._RFpop();
