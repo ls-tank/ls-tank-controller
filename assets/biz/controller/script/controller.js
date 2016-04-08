5b6d86294919d7fb58dbefb34c4eff2567e35fc1
lishengzxc <eric@lishengcn.cn>
2016-04-08 20:00:34 +0800

    add continue

--- assets/biz/controller/script/controller.js
+++ assets/biz/controller/script/controller.js
@@ -1,14 +1,41 @@
 var Connect = require('connect');
 
+var loseAction = {
+    show: cc.moveTo(0.5, cc.p(0, 0)).easing(cc.easeIn(3.0)),
+    hide: cc.moveTo(0.5, cc.p(0, 640)).easing(cc.easeIn(3.0))
+};
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
-    
+        lose: {
+            default: null,
+            type: cc.Node
+        },
+        direct: {
+            default: null,
+            type: cc.Node
+        },
+        fire: {
+            default: null,
+            type: cc.Node
+        }
     },
 
     onLoad: function() {
+        Connect.on('c-boom', event => {
+            this.lose.runAction(loseAction.show);    
+        });
+    },
     
+    nextHandler: function() {
+        this.lose.runAction(loseAction.hide);
+        Connect.emit('c-next');
+    },
+    
+    equipHandler: function() {
+        
     },
     
     backHandler: function() {
